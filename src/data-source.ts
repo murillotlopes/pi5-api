a5de4c1092c42f6792cc5145a718d77fb81fc48f
murillotlopes <murillo.tlopes@hotmail.com>
2023-06-17 09:02:03 -0300

    style(appsource): formatação

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,24 +6,17 @@ import { DataSource } from "typeorm"
 config()
 
 export const AppDataSource =
-  process.env.NODE_DEV === 'test'
-    ? new DataSource({
-      type: 'sqlite',
-      database: ':memory:',
-      entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
-      synchronize: true
-    })
-    : new DataSource({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD as string,
-      database: process.env.POSTGRES_DB,
-      ssl: { rejectUnauthorized: false },
-      entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
-      migrations: [path.join(__dirname, "/migrations/**/*.{ts,js}")],
-    })
+  new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD as string,
+    database: process.env.POSTGRES_DB,
+    ssl: { rejectUnauthorized: false },
+    entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
+    migrations: [path.join(__dirname, "/migration/**/*.{ts,js}")],
+  })
 
 // export const AppDataSource = new DataSource({
 //   type: "postgres",
