58c362100d8e1bc463ede3f10a5923e316a3171c
murillotlopes <murillo.tlopes@hotmail.com>
2023-06-27 19:28:32 -0300

    feat(rotas): rotas base

--- src/project/rotaApi.ts
+++ src/project/rotaApi.ts
@@ -1,8 +1,12 @@
 import { Express } from 'express'
 import usuarioRota from './usuario/usuario.rota'
+import operacaoRota from './operacao/operacao.rota'
+import tituloRota from './tituloInvestimento/titulo.rota'
 
 const registroRotas = (app: Express): void => {
     app.use('/usuario', usuarioRota)
+    app.use('/operacao', operacaoRota)
+    app.use('/titulo', tituloRota)
 }
 
 export default registroRotas
