227da75db9d46987d0d3dd116bc1d9902084eecf
murillotlopes <murillo.tlopes@hotmail.com>
2023-10-16 21:53:52 -0300

    feat(providers): criação de acesso a api de analise knn

---
+++ src/providers/apiKnn.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosInstance } from "axios";
+
+class ApiKnn {
+  private api: AxiosInstance
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: '127.0.0.1:5000/api/knn'
+    })
+  }
+
+  public async analise(payload: {}) {
+    return await this.api.post('/analise', payload)
+  }
+
+}
+
+export default new ApiKnn
