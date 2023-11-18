248bfcbb37a74a4542f91c4808b9c523f79eef1a
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 14:16:56 -0300

    sincronização das tabelas desativado e pequenos ajustes na configuração da conexão com banco

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,14 +9,15 @@ export const AppDataSource =
   new DataSource({
     type: "postgres",
     host: "localhost",
-    // port: process.env.PORT_DB as unknown as number,
-    // username: process.env.POSTGRES_USER,
-    // password: process.env.POSTGRES_PASSWORD as string,
-    // database: process.env.POSTGRES_DB,
-    url: process.env.DATABASE_URL,
+    port: process.env.PORT_DB as unknown as number,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD as string,
+    database: process.env.POSTGRES_DB,
+    // url: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     entities: [path.join(__dirname, "/entities/**/*.{ts,js}")],
     migrations: [path.join(__dirname, "/migration/**/*.{ts,js}")],
+    synchronize: false
   })
 
 // export const AppDataSource = new DataSource({
