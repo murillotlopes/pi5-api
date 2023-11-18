0af4487315d23025f11313d16e7c0998e9bb8999
murillotlopes <murillo.tlopes@hotmail.com>
2023-11-18 14:17:41 -0300

    inclusão do serviço CORS na iniciallização do projeto

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import registroRotas from "./project/rotaApi";
+import cors from "cors"
 
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 registroRotas(app)
 
 export default app
